015aa869ac94e028a1cfbd01a7477a516c5364b4
olegtim <oleg@webof3.com>
2020-09-28 18:13:41 -0700

    Initial

---
+++ src/main/java/com/solutions/app/SolutionsAppApplication.java
@@ -0,0 +1,13 @@
+package com.solutions.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutionsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolutionsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solutions/app/SolutionsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solutions.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutionsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
